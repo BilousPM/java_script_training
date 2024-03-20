9d9134b801d5ae2b31e8fc20951077a29ec49347
Petro Bilous <bilouspm@gmail.com>
2024-03-20 11:38:57 -0600

    fix

---
+++ src/js/first_lesson.js
@@ -0,0 +1,9 @@
+const adge = 10;
+const userName = 'Petro';
+const isOpen = true;
+const shouldConfirm = false;
+
+console.log(adge);
+console.log(userName);
+console.log(isOpen);
+console.log(shouldConfirm);
