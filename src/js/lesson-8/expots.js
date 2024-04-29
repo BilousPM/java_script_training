f622295bdd7e7ddbb68caed70d8ec4d371b14eae
BilousPM <bilouspm@gmail.com>
2024-04-29 10:43:08 -0600

    Summary

---
+++ src/js/lesson-8/expots.js
@@ -0,0 +1,39 @@
+// const fetchUser = user => console.log('it is User');
+
+// const fetchUserById = id => {
+//   console.log('fetch user by Id');
+// };
+
+// const updateUserById = id => {
+//   console.log('upDateUserById');
+// };
+// #2
+// export default { fetchUser, fetchUserById, updateUserById };
+
+// #3 іменованний експорт
+// export const fetchUser = user => console.log('it is User');
+
+// export const fetchUserById = id => {
+//   console.log('fetch user by Id');
+// };
+
+// export const updateUserById = id => {
+//   console.log('upDateUserById');
+// };
+
+// export const x = 4;
+// export const y = 10;
+
+// #4
+export const fetchUser = user => console.log('it is User');
+
+export const fetchUserById = id => {
+  console.log('fetch user by Id');
+};
+
+export const updateUserById = id => {
+  console.log('upDateUserById');
+};
+
+export const x = 4;
+export const y = 10;
---
+++ src/js/lesson-8/modules.js
@@ -0,0 +1,31 @@
+// import qwe from './file-for-import';
+// console.log(qwe);
+
+// #2
+// import apiServis from './expots';
+// console.log(apiServis);
+
+// #3 шменнованний експорт
+// import {
+//   fetchUser,
+//   fetchUserById,
+//   updateUserById,
+//   x,
+//   y as value,
+// } from './expots';
+// console.log(fetchUser);
+// console.log(fetchUserById);
+// console.log(updateUserById);
+// console.log(x);
+// console.log(value);
+
+// #4 забирає все що є в папці
+// імпорт пространства імен
+
+import * as apserrvis from './expots';
+console.log(apserrvis);
+
+// #5 Абсолютний імпорт
+
+import Joi from 'joi';
+console.log(Joi);
