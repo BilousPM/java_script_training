bf3d76db7f21aa75dac52da1927fa157317722c9
BilousPM <111942988+BilousPM@users.noreply.github.com>
2024-05-06 04:32:13 +0000

    Deploying to gh-pages from @ BilousPM/java_script_training@b3b29e7da9302c08cbb03bb4e0d38b1af4990d02 🚀

---
+++ distributionevents.88496225.js
@@ -0,0 +1,2 @@
+const o={name:"Mango",age:3,isHappy:!0};console.log(o);const n=JSON.stringify(o);console.log(typeof n),console.log(n);const s=JSON.parse(n);console.log(s),console.log(n===s);
+//# sourceMappingURL=distributionevents.88496225.js.map
