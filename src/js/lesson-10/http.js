44339a696182893ec9120034353393c081cc3ab9
BilousPM <bilouspm@gmail.com>
2024-05-19 12:58:22 -0600

    adds chenges\

---
+++ src/js/lesson-10/http.js
@@ -0,0 +1,28 @@
+// import pokemonCard from '../../templates/pocemon-card.hbs';
+
+//  Web Serwer - динамічно генерує html на сервері
+// API Server - просто віддає данні в форматі JSON, а вже клієнт (браузер) відмальовує сторінку..
+// Основні HTTP ьетоди
+// GET - отримати данні;
+// POST - відправити данні, і створити нові;
+// PUT and PATCH - відправити данні, і обновити існуючі;
+// DELETE - видалити існуючі данні
+
+// HTTP Status Codes Glossary
+// httpstatuses.com
+// https://httpstatuses.com
+
+//методи парса двнних: text(), json(), blob()
+
+// fetch('https://pokeapi.co/api/v2/pokemon/2')
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(pokemon => {
+//     console.log(pokemon);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+// console.log(pokemonCard);
